bcda5f265ec2003315a61c88a5936327f3628ab5
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-11 23:26:42 +0900

    範囲外のurlが直接入力された時に、0に変更する機能を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import "../styles/globals.css"
 import "github-markdown-dracula-css"
 
 import { useStoreProvider } from '../utils/use-store-provider'
+import { replaceAnchorLinkNumber } from '../utils/anchor-link'
 
 const [StoreProvider, StoreHooks] = useStoreProvider(() => (
   { state: { section: [0, 1, 2] } }
@@ -23,15 +24,19 @@ const App = ({ Component, pageProps }: AppProps) => {
   const store = useStore()
 
   useEffect(() => {
-    console.log("onload")
-
     if (!window.location.hash) {
       window.location.hash = "section0"
     }
 
-    if (store.state.section.includes(Number(window.location.hash.match(/\d+/)?.[0] ?? 0))) {
-      window.location.hash = "section0"
-    }
+    replaceAnchorLinkNumber(n => (
+      store.state.section.includes(n) ? n : 0
+    ))
+
+    window.addEventListener("hashchange", () => {
+      replaceAnchorLinkNumber(n => (
+        store.state.section.includes(n) ? n : 0
+      ))
+    })
   }, [])
 
   return (
