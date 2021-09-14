b91a5b066076a45beacce1eaf72891839f47f5ef
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-14 19:53:52 +0900

    スタイルを整理

--- assets/steps/step1.tsx
+++ assets/steps/step1.tsx
@@ -16,53 +16,57 @@ export const Page = () => {
   const { register, handleSubmit, setValue } = useForm<FormType>()
 
   return (
-    <div className="flex flex-col items-center">
-      <h1>Q1. 幼馴染みがいる場所は・・？</h1>
-      <form onSubmit={handleSubmit((data) => {
-        if (!data.select) {
-          alert("回答を入力してね！")
-        } else if (data.select !== Answer) {
-          alert("回答が違うみたいだ・・・")
-        } else {
-          alert("おめでとう！正解！")
-          addNextStepToAvailableSections()
-          setTimeout(() => {
-            moveToAvailableSection(i => i + 1)
-          }, 100)
-        }
-      })}>
-        <div>
-          <input type="radio" value={0} {...register("select")} />
-          <span>1. 場所A"</span>
-        </div>
-        <div>
-          <input type="radio" value={1} {...register("select")} />
-          <span>2. 場所B"</span>
-        </div>
-        <div>
-          <input type="radio" value={2} {...register("select")} />
-          <span>3. 場所C"</span>
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="px-2 py-1 m-1 bg-gray-100 hover:bg-gray-300 text-gray-700 rounded"
-          >
-            決定！
-          </button>
-          <button
-            type="button"
-            className="px-2 py-1 m-1 bg-gray-100 hover:bg-gray-300 text-gray-700 rounded"
-            onClick={() => {
-              const result = confirm("本当に回答を確認する?")
-              if (result) { setValue("select", Answer) }
-            }}
-          >
-            回答をみる
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className="flex flex-col items-center" onSubmit={handleSubmit((data) => {
+      if (!data.select) {
+        alert("回答を入力してね！")
+      } else if (data.select !== Answer) {
+        alert("回答が違うみたいだ・・・")
+      } else {
+        alert("おめでとう！正解！")
+        addNextStepToAvailableSections()
+        setTimeout(() => {
+          moveToAvailableSection(i => i + 1)
+        }, 100)
+      }
+    })}>
+      <div>
+        <h1>Q1. 幼馴染みがいる場所は・・？</h1>
+      </div>
+
+      <div className="text-2xl">
+        {[0, 1, 2].map(i => (
+          <div key={i}>
+            <input
+              type="radio"
+              value={i}
+              className="w-4 h-4 align-middle"
+              style={{ margin: "0 0.3em" }}
+              {...register("select")}
+            />
+            <span>{i + 1}. 場所A</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="text-xl mt-2">
+        <button
+          type="submit"
+          className="px-2 py-1 m-1 bg-gray-100 hover:bg-gray-300 text-gray-700 rounded"
+        >
+          決定
+        </button>
+        <button
+          type="button"
+          className="px-2 py-1 m-1 bg-gray-100 hover:bg-gray-300 text-gray-700 rounded"
+          onClick={() => {
+            const result = confirm("本当に回答を確認する?")
+            if (result) { setValue("select", Answer) }
+          }}
+        >
+          回答をみる
+        </button>
+      </div>
+    </form>
   )
 }
 
