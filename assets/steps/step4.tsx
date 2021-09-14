9c64aadb4b6889a23dde42742d9ab08178490912
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-15 00:59:24 +0900

    step4を実装

--- assets/steps/step4.tsx
+++ assets/steps/step4.tsx
@@ -1,5 +1,48 @@
-export default (
-  <div>
-    <h1>謎解き Part.4</h1>
-  </div>
-)
+import { useEffect, useState } from 'react';
+import { DraggableList } from '../../components/DroppableList';
+import { StepForm } from '../../components/StepForm';
+
+function shuffle<T>(array: T[]) {
+  return array.sort(() => Math.random() - 0.5)
+}
+
+const Items = shuffle(Array.from("テレポーター"))
+const Answer = "テレポーター"
+
+function useIsFirst(): boolean {
+  const [flag, setFlag] = useState<boolean>(true)
+  useEffect(() => { setFlag(false) }, [])
+  return flag
+}
+
+export const Page = () => {
+  const isFirst = useIsFirst()
+  const [items, setItems] = useState(Items)
+
+  const Header = (
+    <div>
+      <h1>Q4. テレポーターの場所は・・？</h1>
+    </div>
+  )
+
+  return (
+    <StepForm value={Items.join("")} answer={Answer} header={Header} >
+      {({ setValue }) => (isFirst ? null : (
+        <DraggableList items={items} onDragEnd={items => {
+          setItems(items)
+          setValue("value", items.join(""))
+        }} container={(
+          <div className="flex justify-center w-80 rounded border-2 bg-gray-500 border-gray-100"></div>
+        )}>
+          {(item) => (
+            <div className="px-2 py-1 m-2 text-center flex-grow rounded bg-gray-100 text-gray-700">
+              {item}
+            </div>
+          )}
+        </DraggableList>
+      ))}
+    </StepForm>
+  )
+}
+
+export default (<div><Page /></div>)
