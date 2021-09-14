3b30333816402d8c08cafdc2aa636b596b429882
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-14 15:31:49 +0900

    StartPageの中身を作成

--- assets/steps/step0.tsx
+++ assets/steps/step0.tsx
@@ -1,6 +1,26 @@
-export default (
-  <div>
-    <h1>ようこそ！</h1>
-    <h2>スワイプなどで次に進んでね</h2>
-  </div>
-)
+import { useAvailableSections } from "../../lib/use-available-sections"
+
+const Page = () => {
+  const {
+    moveToAvailableSection,
+    addNextStepToAvailableSections
+  } = useAvailableSections()
+ 
+  return (
+    <div className="flex flex-col items-center">
+      <h1>ようこそ！</h1>
+      <button className="
+        text-3xl m-2 px-4 py-2 rounded-lg border-white border-2 
+        bg-gray-100 hover:bg-gray-700 
+        text-gray-700 hover:text-gray-100
+      " onClick={() => {
+        addNextStepToAvailableSections()
+        moveToAvailableSection(i => i + 1)
+      }}>
+        Let's Start!
+      </button>
+    </div>
+  )
+}
+
+export default <Page />
