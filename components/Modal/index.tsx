7e540f4db5e5c479c3540c139afc8342f28cacec
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-18 23:16:22 +0900

    EasyTransitionをファイル分離

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,51 +1,8 @@
-import React, { useEffect, useState } from "react"
-import { Transition } from '@headlessui/react'
+import React from "react"
+import { EasyTransition } from "../EasyTransition"
 
 interface ModalContainerProps {
   children?: React.ReactElement
-}
-
-function useCacheState<T>(state: T) {
-  const [cache, setCache] = useState<T | undefined>()
-
-  useEffect(() => {
-    if (state) { setCache(state) }
-  }, [state])
-
-  return cache
-}
-
-interface EasyTransitionProps {
-  children?: React.ReactNode
-
-  transition?: string
-  from?: string
-  to?: string
-
-  enter?: string
-  enterFrom?: string
-  enterTo?: string
-  leave?: string
-  leaveFrom?: string
-  leaveTo?: string
-}
-
-function EasyTransition({ children, ...props }: EasyTransitionProps) {
-  const cache = useCacheState(children)
-
-  return (
-    <Transition
-      show={Boolean(children)}
-      enter={props.enter ?? props.transition}
-      enterFrom={props.enterFrom ?? props.from}
-      enterTo={props.enterTo ?? props.to}
-      leave={props.leave ?? props.transition}
-      leaveFrom={props.leaveFrom ?? props.to}
-      leaveTo={props.leaveTo ?? props.from}
-    >
-      {cache}
-    </Transition>
-  )
 }
 
 export const ModalContainer = ({ children }: ModalContainerProps) => {
