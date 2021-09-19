d4b5fb4430ff772ebbb99c11cbcf403b89840882
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-19 18:22:11 +0900

    InsertClassNameを実装

---
+++ components/InsertClassName/index.tsx
@@ -0,0 +1,19 @@
+import { CloneElement } from "../CloneElement"
+
+function insertText(where: "before" | "after", text1: string, text2: string) {
+  return (where === "after" ? `${text1} ${text2}` : `${text2} ${text1}`).trim()
+}
+
+interface InsertClassNameProps {
+  where: "before" | "after"
+  className: string
+  children: React.ReactElement
+}
+
+export function InsertClassName({ children, className, where }: InsertClassNameProps) {
+  return (<CloneElement
+    element={children as JSX.Element}
+    props={{ className: insertText(where, className, children.props.className)}}
+    children={children.props.children}
+  />)
+}
