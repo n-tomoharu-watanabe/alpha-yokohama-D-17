64a6196e660f5a617ec3e75acf6aa399ff2cf52a
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-19 18:21:56 +0900

    Modalのstyleを調整

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -8,7 +8,7 @@ interface ModalTransitionProps {
 export const ModalTransition = ({ children }: ModalTransitionProps) => {
   return (
     <EasyTransition
-      transition="transition-opacity duration-300"
+      transition="transition-opacity duration-150"
       from="opacity-0"
       to="opacity-100"
     >
@@ -25,7 +25,7 @@ interface ModalContainerProps {
 export function ModalContainer({ children, close }: ModalContainerProps) {
   return (
     <div className="modal fixed w-full h-full top-0 left-0 flex items-center justify-center">
-      <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" onClick={() => close?.()} />
+      <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-70" onClick={() => close?.()} />
       <div className="modal-container bg-white w-11/12 max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
         {children ?? "Hello!"}
       </div>
