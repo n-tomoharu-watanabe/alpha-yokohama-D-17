8515cd547d7cc2e4c5a1908872a5e75629c10ff5
n-tomoharu-watanabe <tomoharu_20n1101363@nnn.ed.jp>
2021-09-21 04:31:37 +0900

    Q3にCard型の回答形式を実装

--- assets/steps/step3.tsx
+++ assets/steps/step3.tsx
@@ -1,28 +1,69 @@
+import { MouseEventHandler, useMemo } from 'react';
 import { StepForm } from '../../components/StepForm';
 
 const Answer = "3"
 
+const Modal = {
+  hint: "前のページに犯人の特徴が・・？",
+  answer: "No.4 犯人D"
+}
+
+interface ImgCardProps {
+  src: string
+  title: React.ReactNode
+  select: boolean
+  onClick: MouseEventHandler
+  children?: React.ReactNode
+}
+
+const ImgCard = ({ src, title, select, onClick, children }: ImgCardProps) => {
+  return (
+    <div className={`
+      m-1 rounded-xl text-xl text-black overflow-hidden bg-none border-4 border-blue-300
+      ${select ? "border-opacity-100" : "border-opacity-0"}
+    `} onClick={onClick}>
+      <div className="bg-gray-50 hover:bg-gray-200">
+        <div className="relative" style={{ height: "35vh", maxWidth: "23vw" }}>
+          <img src={src} className="h-full w-full" />
+          <div className="absolute left-0 bottom-0 px-2 pb-2">
+            <span className="text-3xl text-white">
+              {title}
+            </span>
+          </div>
+        </div>
+        <div className="px-2 pb-2">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const Page = () => {
   const Header = (
     <div>
-      <h1>Q3. 犯人の名前は・・？</h1>
+      <h1>Q3. 犯人はどの人・・？</h1>
     </div>
   )
 
   return (
-    <StepForm answer={Answer} header={Header} >
-      {({ register }) => ([0, 1, 2, 3].map(i => (
-        <div key={i} className="text-2xl">
-          <input
-            type="radio"
-            value={i}
-            className="w-4 h-4 align-middle"
-            style={{ margin: "0 0.3em" }}
-            {...register("value")}
-          />
-          <span>{i + 1}. 犯人{"ABCD"[i]}</span>
+    <StepForm answer={Answer} header={Header} modal={Modal} >
+      {({ getValues, setValue, watch }) => (useMemo(() => (
+        <div className="flex">
+          {[0, 1, 2, 3].map(i => (
+            <ImgCard
+              src="https://placehold.jp/888888/ffffff/400x600.png?text=No%20Image"
+              title={`犯人${"ABCD"[i]}`}
+              select={getValues("value") === String(i)}
+              onClick={() => setValue("value", String(i))}
+            >
+              <div>1. 特徴1</div>
+              <div>2. 特徴2</div>
+              <div>3. 特徴3</div>
+            </ImgCard>
+          ))}
         </div>
-      )))}
+      ), [watch("value")]))}
     </StepForm>
   )
 }
